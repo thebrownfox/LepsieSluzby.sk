047f89e65df7bf84bdf8c4bf73a3065bdb35a34a
Marek Brenčič <marek.brencic@eea.sk>
2019-08-09 17:42:52 +0200

    [WIP] table

--- src/components/table.vue
+++ src/components/table.vue
@@ -1,13 +1,48 @@
 <template>
-    
+    <table class="govuk-table">
+        <caption class="govuk-table__caption govuk-heading-m">{{ options.caption }}</caption>
+        <thead class="govuk-table__head">
+            <tr class="govuk-table__row">
+                <th
+                    class="govuk-table__header"
+                    :class="{'govuk-table__header--numeric': header.type === Number}"
+                    v-for="(header, index) in data.head"
+                    :key="index"
+                    scope="col"
+                >{{ header.title}}</th>
+            </tr>
+        </thead>
+        <tbody class="govuk-table__body" id="listing">
+            <tr class="govuk-table__row" v-for="(row, index) in data.body" :key="index">
+                <td class="govuk-table__cell" :class="{'govuk-table__cell--numeric': cell.type === Number}" v-for="(cell, index) in row" :key="index"></td>
+            </tr>
+        </tbody>
+    </table>
 </template>
 
 <script>
-export default {
-
+//TODO: This is for future?
+function tableParams() {
+    this.caption = "";
 }
+export default {
+    props: {
+        options: {
+            type: Object,
+            default: () => ({
+                caption: "",
+            })
+        },
+        data: {
+            type: Object,
+            default: () => ({
+                head: [],
+                body: [],
+            })
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
--- src/views/home.vue
+++ src/views/home.vue
@@ -8,23 +8,7 @@
                         <nd-thumbnail url="/podnet"></nd-thumbnail>
                     </div>
                 </div>
-                <table class="govuk-table">
-                    <caption class="govuk-table__caption govuk-heading-m">Čo aktuálne riešime</caption>
-                    <thead class="govuk-table__head">
-                        <tr class="govuk-table__row">
-                            <th class="govuk-table__header" scope="col">Podnet</th>
-                            <th
-                                class="govuk-table__header govuk-table__header--numeric"
-                                scope="col"
-                            >Kategória</th>
-                            <th
-                                class="govuk-table__header govuk-table__header--numeric"
-                                scope="col"
-                            >Dátum</th>
-                        </tr>
-                    </thead>
-                    <tbody class="govuk-table__body" id="listing"></tbody>
-                </table>
+                <nd-table :data="tableData" :options="tableOptions"></nd-table>
                 <nd-button>Zobraziť viac</nd-button>
             </main>
         </div>
@@ -32,7 +16,35 @@
 </template>
 
 <script>
-export default {};
+//TODO: Add notification about new jira issues
+export default {
+    data() {
+        return {
+            tableOptions: {
+                caption: "Čo aktuálne riešime"
+            },
+            jiraData: null
+        };
+    },
+    computed: {
+        tableData: function() {
+            let data = this.jiraData;
+        }
+    },
+    methods: {
+        getJiraData: function() {
+            const searchURL =
+                "https://lepsiesluzby.sk/jira/rest/api/2/search?jql=project%20%3D%20SDM%20%20AND%20component%20%3D%20e-services%20ORDER%20BY%20created%20DESC&fields=summary,created,customfield_10204&maxResults=5";
+            let jiraData;
+
+
+            return jiraData;
+        }
+    },
+    mounted: function() {
+        this.jiraData = getJiraData();
+    }
+};
 </script>
 
 <style>
